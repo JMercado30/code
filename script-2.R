6d9b89394d098d8b625509ea6c6e448d8ba9cbeb
JMercado30 <j.mercado30@ncf.edu>
2026-08-20 03:51:02 +0000

    finish code tutorial

--- script-2.R
+++ script-2.R
@@ -1,3 +1,31 @@
-library (tidyverse)
+library(tidyverse)
 
-# Had boneless wings from wingstop
+hist_p <- ggplot(diamonds, aes(x = carat)) +
+	geom_histogram(
+		binwidth = 0.05,
+		boundary = 0,
+		fill = "darkgray",
+		color = "black"
+	) +
+	scale_y_continuous(
+		breaks = seq(0, 6000, by = 2000)
+	) +
+	coord_cartesian(ylim = c(0, 7000)) +
+	labs(
+		title = "Histogram of Carat (Weight) among 50,000 Diamonds",
+		subtitle = "Carats just at or above meaningful numbers are very common",
+		x = "Carat size",
+		y = "Number of diamonds",
+		caption = "diamonds dataset from ggplot2 package"
+	) +
+	theme_gray()
+
+ggsave(
+	file = "diamonds.png",
+	plot = hist_p,
+	width = 6,
+	height = 4,
+	units = "in",
+	dpi = 300
+)
+
